7929d412b120642c49a10c34be7f484d3f9eb8ec
Odin Wermeille <odin.wermeille@heig-vd.ch>
2025-03-08 15:59:20 +0100

    Completed tic-tac-toe tutorial

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './setThing';
+import App from './ticTacToe';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
