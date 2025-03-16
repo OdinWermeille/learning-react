4a4c4273d8be7383025009d9b91a92bca64aa513
Odin Wermeille <odin.wermeille@heig-vd.ch>
2025-03-16 16:35:23 +0100

    Started experimenting with sandbox

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './product';
+import App from './installationSandbox';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
