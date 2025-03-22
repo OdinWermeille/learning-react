ddf227d2034a7e4969f034ec470969bad7972083
Odin Wermeille <odin.wermeille@heig-vd.ch>
2025-03-22 10:06:20 +0100

    Displayed .tsx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './installationSandbox';
+import App from './test-ts.tsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
